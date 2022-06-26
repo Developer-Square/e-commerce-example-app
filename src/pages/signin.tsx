cd9f9979e81b21d7e0798f941818edc7e993ee23
ryann254 <keddelyronjoz@gmail.com>
2022-06-26 12:07:19 +0300

    feat: added forgot password page and a components folder for the sigin page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable tailwindcss/no-custom-classname */
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import TextBox from 'modules/common/TextBox';
 import Image from 'next/image';
 import React, { useState } from 'react';
@@ -8,24 +11,40 @@ import { animated, config, useSpring } from 'react-spring';
 import JewelleryIcon from '@/public/assets/images/icons/jewelry.png';
 
 const Signin = () => {
-  const [opacity, setOpacity] = useState<Record<string, number>>({
+  const [loginOpacity, setLoginOpacity] = useState<Record<string, number>>({
     to: 1,
     from: 0,
   });
+  const [signUpOpacity, setSignUpOpacity] = useState<Record<string, number>>(
+    {}
+  );
+  const [forgotPasswordOpacity, setForgotPasswordOpacity] = useState<
+    Record<string, number>
+  >({});
   const [pageState, setPageState] = useState('signin');
 
   // When the user switches to the signup page, hide the login input fields.
   const loginProps = useSpring({
-    to: { opacity: opacity.to },
-    from: { opacity: opacity.from },
+    to: { opacity: loginOpacity.to },
+    from: { opacity: loginOpacity.from },
     reset: true,
     reverse: false,
     delay: 300,
     config: config.molasses,
   });
+
   const signUpProps = useSpring({
-    to: { opacity: opacity.from },
-    from: { opacity: opacity.to },
+    to: { opacity: signUpOpacity.to },
+    from: { opacity: signUpOpacity.from },
+    reset: true,
+    reverse: false,
+    delay: 300,
+    config: config.molasses,
+  });
+
+  const forgotPasswordProps = useSpring({
+    to: { opacity: forgotPasswordOpacity.to },
+    from: { opacity: forgotPasswordOpacity.from },
     reset: true,
     reverse: false,
     delay: 300,
@@ -34,7 +53,7 @@ const Signin = () => {
   return (
     <section
       className={`flex ${
-        pageState === 'signin' ? 'h-screen' : 'h-full'
+        pageState === 'signup' ? 'h-full' : 'h-screen'
       } w-full items-center justify-center bg-[#dde5f4]`}
     >
       <div className="card w-96 bg-[#f1f7fe] text-slate-800">
@@ -64,7 +83,8 @@ const Signin = () => {
                 />
               </animated.div>
             </>
-          ) : (
+          ) : null}{' '}
+          {pageState === 'signup' ? (
             <>
               <animated.div style={signUpProps} className="w-full">
                 <TextBox title="Username" type="text" placeholder="John Doe" />
@@ -91,23 +111,69 @@ const Signin = () => {
                 />
               </animated.div>
             </>
-          )}
+          ) : null}
+          {pageState === 'forgot' ? (
+            <>
+              <animated.div style={forgotPasswordProps} className="w-full">
+                <div className="alert alert-info mb-4 text-white shadow-lg">
+                  <div>
+                    <FontAwesomeIcon
+                      icon={faCircleInfo}
+                      style={{ color: '#fff' }}
+                    />
+                    <span>
+                      You'll receive an email with a link to reset your passord.
+                    </span>
+                  </div>
+                </div>
+                <TextBox
+                  title="Email Address"
+                  type="text"
+                  placeholder="Username@gmail.com"
+                />
+              </animated.div>
+            </>
+          ) : null}
           <div className="card-actions mt-5 flex w-full justify-center">
             <button className="btn w-full rounded-3xl bg-[#3e4684]">
-              {pageState === 'signin' ? 'Login' : 'Signup'}
+              {pageState === 'signin' ? 'Login' : null}
+              {pageState === 'signup' ? 'Signup' : null}
+              {pageState === 'forgot' ? 'Send' : null}
             </button>
+            {pageState === 'forgot' ? (
+              <button
+                className="btn mt-2 w-full rounded-3xl bg-red-500"
+                onClick={() => {
+                  setLoginOpacity({ to: 1, from: 0 });
+                  setForgotPasswordOpacity({ to: 0, from: 1 });
+                  setPageState('signin');
+                }}
+              >
+                Back
+              </button>
+            ) : null}
             {pageState === 'signin' ? (
               <div className="my-5 flex w-full justify-between text-sm font-semibold">
                 <span
                   className="cursor-pointer"
                   onClick={() => {
-                    setOpacity({ to: 0, from: 1 });
+                    setLoginOpacity({ to: 0, from: 1 });
+                    setSignUpOpacity({ to: 1, from: 0 });
                     setPageState('signup');
                   }}
                 >
                   Signup
                 </span>
-                <span className="cursor-pointer">Forgot Password</span>
+                <span
+                  className="cursor-pointer"
+                  onClick={() => {
+                    setLoginOpacity({ to: 0, from: 1 });
+                    setForgotPasswordOpacity({ to: 1, from: 0 });
+                    setPageState('forgot');
+                  }}
+                >
+                  Forgot Password
+                </span>
               </div>
             ) : null}
             {pageState === 'signup' ? (
@@ -117,7 +183,8 @@ const Signin = () => {
                   <p
                     className="cursor-pointer pl-1 font-bold"
                     onClick={() => {
-                      setOpacity({ to: 1, from: 0 });
+                      setLoginOpacity({ to: 1, from: 0 });
+                      setSignUpOpacity({ to: 0, from: 1 });
                       setPageState('signin');
                     }}
                   >
