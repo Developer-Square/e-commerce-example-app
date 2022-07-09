d687231a65e255a8f6a477040770432e18d671ed
ryann254 <keddelyronjoz@gmail.com>
2022-07-09 10:25:27 +0300

    feat: added search bar and dropdown

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -2,16 +2,23 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 import {
   faBlog,
+  faCartArrowDown,
   faCartShopping,
   faCircleQuestion,
   faCreditCard,
+  faHeart,
   faStore,
 } from '@fortawesome/free-solid-svg-icons';
 import type { ReactNode } from 'react';
 import { useMemo, useState } from 'react';
 import { easings } from 'react-spring';
 
-import { config, ExpandableNavItem, useSpring } from '@/lib/common';
+import {
+  config,
+  ExpandableNavItem,
+  FontAwesomeIcon,
+  useSpring,
+} from '@/lib/common';
 import { AppConfig } from '@/utils/AppConfig';
 
 type IMainProps = {
@@ -188,7 +195,46 @@ const Main = (props: IMainProps) => {
                 </a>
               </li>
             </nav>
-            <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0"></div>
+            <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
+              <div className="dropdown">
+                <button className="btn btn-ghost btn-circle mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                  </svg>
+                </button>
+                <ul
+                  tabIndex={0}
+                  className="dropdown-content menu rounded-box w-52 bg-base-100 p-2 shadow"
+                >
+                  <li>
+                    <input
+                      type="text"
+                      placeholder="Search..."
+                      className="input-bordered input-info input w-full max-w-xs bg-transparent"
+                    />
+                  </li>
+                </ul>
+              </div>
+
+              <button className="mr-5">
+                <FontAwesomeIcon icon={faHeart} />
+              </button>
+              <button className="mr-5">
+                <FontAwesomeIcon icon={faCartArrowDown} />
+                <span className="ml-1">$ 0.00</span>
+              </button>
+            </div>
           </div>
         </div>
 
