59ec8dc355b6d4309f87337815512fd1320ad424
ryann254 <keddelyronjoz@gmail.com>
2022-07-23 13:44:47 +0300

    fix: Fixed the one feature in 768px mode

--- src/lib/homepage/feature-section/FeatureSection.tsx
+++ src/lib/homepage/feature-section/FeatureSection.tsx
@@ -1,16 +1,20 @@
 /* eslint-disable tailwindcss/no-custom-classname */
 import React from 'react';
 
-const FeatureSections = () => {
+type Props = {
+  content: string;
+  image: string;
+};
+
+const Section = ({ content, image }: Props) => {
   return (
-    <div className="container mx-auto mt-24 px-4">
-      <div className="column-1">
-        <div className="flex flex-col">
-          <img src="/assets/images/banner/banner-1.jpg" alt="feature section" />
+    <div className="mb-10 flex flex-col items-center md:flex-row">
+      <div className="image-text relative w-full">
+        <div className="absolute top-1/4 left-10 w-full max-w-xs">
           <p className="mb-2.5 max-w-full pt-6 text-4xl font-bold text-[#111]">
-            Clothing Collections 2030
+            {content}
           </p>
-          <li className="">
+          <li className="navbar-item w-32">
             <a
               href="#"
               className="letter-spacing py-1 text-sm font-bold uppercase text-[#111]"
@@ -19,6 +23,23 @@ const FeatureSections = () => {
             </a>
           </li>
         </div>
+      </div>
+      <img
+        className=""
+        src={`/assets/images/banner/${image}`}
+        alt="feature section"
+      />
+    </div>
+  );
+};
+
+const FeatureSections = () => {
+  return (
+    <div className="container mx-auto mt-24 px-4">
+      <div className="column-1">
+        <Section content="Clothing Collections 2030" image="banner-1.jpg" />
+        <Section content="Accessories" image="banner-2.jpg" />
+        <Section content="Shoe Spring 2030" image="banner-3.jpg" />
       </div>
     </div>
   );
