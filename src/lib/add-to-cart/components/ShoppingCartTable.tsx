94c21e882cd7c9fbca3b68193b4510da96be4571
ryann254 <keddelyronjoz@gmail.com>
2023-02-08 11:19:09 +0300

    feat: add cart items to local storage

--- src/lib/add-to-cart/components/ShoppingCartTable.tsx
+++ src/lib/add-to-cart/components/ShoppingCartTable.tsx
@@ -1,36 +1,23 @@
-import { faX } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
 
-const cartInfo = [
-  { title: 'T-shirt Contrast Pocket', price: '$98.49', image: 'cart-1' },
-  { title: 'Diagonal Textured Cap', price: '$32.50', image: 'cart-2' },
-  { title: 'Basic Flowing Scarf', price: '$47.00', image: 'cart-3' },
-  { title: 'Basic Black Bag', price: '$30.00', image: 'cart-4' },
-];
-
-const CartItem = ({
-  title,
-  price,
-  image,
-}: {
-  title: string;
+interface ICartItems {
+  name: string;
   price: string;
-  image: string;
-}) => (
+  img: string;
+  color?: string;
+}
+
+const CartItem = ({ name, price, img }: ICartItems) => (
   <>
     <tr>
       <td className="flex flex-col py-7 md:flex-row md:items-center ">
         <div>
-          <img
-            src={`/assets/images/shopping-cart/${image}.jpg`}
-            alt="cart-item"
-          />
+          <img src={img} className="h-24 w-24" alt="cart-item" />
         </div>
         <div className="mt-5 md:ml-5 md:mt-0">
-          <h6 className="mb-2.5 w-3/4 text-sm font-semibold md:mb-1">
-            {title}
-          </h6>
+          <h6 className="mb-2.5 w-3/4 text-sm font-semibold md:mb-1">{name}</h6>
           <h5 className="font-bold text-[#0d0d0d]">{price}</h5>
         </div>
       </td>
@@ -57,42 +44,52 @@ const CartItem = ({
   </>
 );
 
-const ShoppingCartTable = () => (
-  <>
-    <div className="lg:mr-6 lg:w-3/4 xl:w-3/5">
-      <table className="mb-7 w-full">
-        <thead>
-          <tr className="text-left text-base uppercase">
-            <th className="pb-6">Product</th>
-            <th className="pb-6">Quantity</th>
-            <th className="pb-6">Total</th>
-            <th className="pb-6"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {cartInfo.map((item, index) => (
-            <CartItem
-              key={index}
-              title={item.title}
-              price={item.price}
-              image={item.image}
-            />
-          ))}
-        </tbody>
-      </table>
-      <div className="shopping-cart-btn flex flex-col">
-        <a
-          href="#"
-          className="shop-now continue-shopping mx-auto inline-block w-3/4 border border-[#e1e1e1] bg-[#fff] py-3.5 px-8 text-center text-[#000] md:mt-5"
-        >
-          Continue shopping
-        </a>
-        <a href="#" className="black-btn update-cart mt-5 w-3/5">
-          Update cart
-        </a>
+const ShoppingCartTable = () => {
+  const [cart, setCart] = useState<ICartItems[]>([]);
+
+  useEffect(() => {
+    const cartItems = localStorage.getItem("cartItems" || "[]");
+    // @ts-ignore
+    setCart(JSON.parse(cartItems));
+  }, []);
+
+  return (
+    <>
+      <div className="lg:mr-6 lg:w-3/4 xl:w-3/5">
+        <table className="mb-7 w-full">
+          <thead>
+            <tr className="text-left text-base uppercase">
+              <th className="pb-6">Product</th>
+              <th className="pb-6">Quantity</th>
+              <th className="pb-6">Total</th>
+              <th className="pb-6"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item, index) => (
+              <CartItem
+                key={index}
+                name={item.name}
+                price={item.price}
+                img={item.img}
+              />
+            ))}
+          </tbody>
+        </table>
+        <div className="shopping-cart-btn flex flex-col">
+          <a
+            href="#"
+            className="shop-now continue-shopping mx-auto inline-block w-3/4 border border-[#e1e1e1] bg-[#fff] py-3.5 px-8 text-center text-[#000] md:mt-5"
+          >
+            Continue shopping
+          </a>
+          <a href="#" className="black-btn update-cart mt-5 w-3/5">
+            Update cart
+          </a>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default ShoppingCartTable;
--- src/lib/homepage/product-section/components/Product.tsx
+++ src/lib/homepage/product-section/components/Product.tsx
@@ -1,14 +1,15 @@
+/* eslint-disable @typescript-eslint/no-shadow */
 import {
   faHeart,
   faSearch,
   faStar,
   faStarHalf,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useRouter } from 'next/router';
-import type { ReactElement } from 'react';
-import React from 'react';
-import { MobileView } from 'react-device-detect';
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/router";
+import type { ReactElement } from "react";
+import React, { useState } from "react";
+import { MobileView } from "react-device-detect";
 
 interface ProductProps {
   image: string;
@@ -19,15 +20,15 @@ interface ProductProps {
 
 const Label = ({ category }: { category: string }) => (
   <>
-    {category !== 'best-sellers' ? (
+    {category !== "best-sellers" ? (
       <span
         className={`label ${
-          category === 'new-arrivals' ? 'text-[#111]' : 'text-[#fff]'
+          category === "new-arrivals" ? "text-[#111]" : "text-[#fff]"
         } ${
-          category === 'new-arrivals' ? 'bg-[#fff]' : 'bg-[#111]'
+          category === "new-arrivals" ? "bg-[#fff]" : "bg-[#111]"
         } absolute left-0 top-5 inline-block px-4 pt-1 pb-0.5 text-xs font-bold uppercase`}
       >
-        {category === 'new-arrivals' ? 'New' : 'Sale'}
+        {category === "new-arrivals" ? "New" : "Sale"}
       </span>
     ) : null}
   </>
@@ -39,36 +40,60 @@ const Product = ({
   price,
   category,
 }: ProductProps): ReactElement => {
+  const [selectedColor, setselectedColor] = useState("bg-[#404a47]");
+
   const convertTitle = (_title: string): string => {
-    return _title.toLowerCase().replace(/\s/g, '-');
+    return _title.toLowerCase().replace(/\s/g, "-");
   };
 
   const router = useRouter();
 
   // Changes the color when a user click on the the colors
-  const handleColorSelect = (e: any, _title: string) => {
+  const handleColorSelect = (e: any, _title: string, color: string) => {
+    setselectedColor(color);
+
     const result = convertTitle(_title);
     const productColorContainer = document.querySelector(
       `#${result}`
     )?.children;
     if (productColorContainer?.length) {
       Array.from(productColorContainer).map((colorItems) => {
-        colorItems.classList.remove('active-select');
+        colorItems.classList.remove("active-select");
         return null;
       });
     }
-    e.currentTarget.classList.add('active-select');
+    e.currentTarget.classList.add("active-select");
   };
 
   const handleProductPush = () => {
-    router.push('/shop-details');
+    router.push("/shop-details");
+  };
+
+  const handleAddToCart = (
+    img: string,
+    name: string,
+    price: string,
+    color: string
+  ) => {
+    // Todo: Add a notification to the user that the item has been added to the cart.
+
+    const cartItems = localStorage.getItem("cartItems");
+    const cartItemsArray = JSON.parse(cartItems || "[]");
+
+    if (cartItemsArray.length) {
+      cartItemsArray.push({ img, name, price, color });
+      localStorage.removeItem("cartItems");
+      localStorage.setItem("cartItems", JSON.stringify(cartItemsArray));
+    } else {
+      localStorage.setItem(
+        "cartItems",
+        JSON.stringify([{ img, name, price, color }])
+      );
+    }
   };
 
   return (
-    <div
-      className="product__item relative mb-10 flex h-full w-full flex-col items-center"
-      onClick={handleProductPush}
-    >
+    <div className="product__item relative mb-10 flex h-full w-full flex-col items-center">
       <Label category={category} />
       <ul className="product__hover invisible absolute -right-52 top-5 opacity-0 transition-all duration-700">
         <li className="relative mb-2.5 bg-white px-2.5 pt-2 pb-1">
@@ -83,9 +108,10 @@ const Product = ({
         </li>
       </ul>
       <img
-        className="w-full bg-center"
+        className="w-full cursor-pointer bg-center"
         src={`/assets/images/product/${image}`}
         alt="product section"
+        onClick={handleProductPush}
       />
       <div className="mt-6 flex w-full justify-between">
         <div className="relative w-full">
@@ -102,6 +128,14 @@ const Product = ({
           <a
             href="#"
             className="add-cart invisible absolute top-0 left-0 text-base font-bold text-[#e53637] opacity-0 transition-all duration-300"
+            onClick={() =>
+              handleAddToCart(
+                `/assets/images/product/${image}`,
+                title,
+                price,
+                selectedColor
+              )
+            }
           >
             + Add To Cart
           </a>
@@ -113,21 +147,27 @@ const Product = ({
             <label
               htmlFor="pc-4"
               className="product-select bg-[#5e64d1]"
-              onClick={(e: any): void => handleColorSelect(e, title)}
+              onClick={(e: any): void =>
+                handleColorSelect(e, title, "bg-[#5e64d1]")
+              }
             >
               <input type="radio" className="invisible absolute" id="pc-4" />
             </label>
             <label
               className="active-select product-select bg-[#404a47]"
               htmlFor="pc-5"
-              onClick={(e: any): void => handleColorSelect(e, title)}
+              onClick={(e: any): void =>
+                handleColorSelect(e, title, "bg-[#404a47]")
+              }
             >
               <input type="radio" className="invisible absolute" id="pc-5" />
             </label>
             <label
               className="product-select bg-[#d5a667]"
               htmlFor="pc-6"
-              onClick={(e: any): void => handleColorSelect(e, title)}
+              onClick={(e: any): void =>
+                handleColorSelect(e, title, "bg-[#d5a667]")
+              }
             >
               <input type="radio" className="invisible absolute" id="pc-6" />
             </label>
