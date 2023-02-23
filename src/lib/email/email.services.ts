a453539c9b4f950623f79b9d31db48899e1d52f9
saisilinus <saisilinus19@gmail.com>
2023-02-23 08:01:37 +0300

    fix: emails with double http

--- src/lib/email/email.services.ts
+++ src/lib/email/email.services.ts
@@ -54,7 +54,7 @@ export const sendResetPasswordEmail = async (
 ): Promise<void> => {
   const subject = 'Reset password';
   // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `http://${envVariables.siteUrl}/reset-password?token=${token}`;
+  const resetPasswordUrl = `${envVariables.siteUrl}/reset-password?token=${token}`;
   const text = `Hi,
     To reset your password, click on this link: ${resetPasswordUrl}
     If you did not request any password resets, then ignore this email.`;
@@ -80,7 +80,7 @@ export const sendVerificationEmail = async (
 ): Promise<void> => {
   const subject = 'Email Verification';
   // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `http://${envVariables.siteUrl}/verify-email?token=${token}`;
+  const verificationEmailUrl = `${envVariables.siteUrl}/verify-email?token=${token}`;
   const text = `Hi ${name},
     To verify your email, click on this link: ${verificationEmailUrl}
     If you did not create an account, then ignore this email.`;
@@ -104,7 +104,7 @@ export const sendSuccessfulRegistration = async (
 ): Promise<void> => {
   const subject = 'Email Verification';
   // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `http://${envVariables.siteUrl}/verify-email?token=${token}`;
+  const verificationEmailUrl = `${envVariables.siteUrl}/verify-email?token=${token}`;
   const text = `Hi ${name},
     Congratulations! Your account has been created. 
     You are almost there. Complete the final step by verifying your email at: ${verificationEmailUrl}
@@ -132,7 +132,7 @@ export const sendAccountCreated = async (
 ): Promise<void> => {
   const subject = 'Account Created Successfully';
   // replace this url with the link to the email verification page of your front-end app
-  const loginUrl = `http://${envVariables.siteUrl}/signin`;
+  const loginUrl = `${envVariables.siteUrl}/signin`;
   const text = `Hi ${name},
     Congratulations! Your account has been created successfully. 
     You can now login at: ${loginUrl}
